a3b10d1052e8078be7e65fa1d54855893c381393
Christian Höppner <chris@mkaito.net>
2020-12-10 17:12:29 +0000

    Add item: Banana

--- src/main/java/net/mkaito/banana/Banana.java
+++ src/main/java/net/mkaito/banana/Banana.java
@@ -5,6 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.mkaito.banana.util.RegistryHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,10 +14,14 @@ public class Banana {
   // Directly reference a log4j logger.
   private static final Logger LOGGER = LogManager.getLogger();
 
+  public static final String MOD_ID = "banana";
+
   public Banana() {
     FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
     FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
     MinecraftForge.EVENT_BUS.register(this);
+
+    RegistryHandler.init();
   }
 
   private void setup(final FMLCommonSetupEvent event) {
---
+++ src/main/java/net/mkaito/banana/util/RegistryHandler.java
@@ -0,0 +1,22 @@
+package net.mkaito.banana.util;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.mkaito.banana.Banana;
+import net.mkaito.banana.items.BananaItem;
+
+public class RegistryHandler {
+  public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Banana.MOD_ID);
+  public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Banana.MOD_ID);
+  // Items
+  public static final RegistryObject<Item> BANANA = ITEMS.register("banana", BananaItem::new);
+
+  public static void init() {
+    ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+  }
+}
